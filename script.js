4ce7f15991b1f3c0ee5c865074034fc11649af32
MelvinSeddik <77323180+MelvinSeddik@users.noreply.github.com>
2021-02-04 17:46:51 +0100

    last changes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/* On récupère les images de la galerie */
+/* MODAL */
 const gallery =  document.querySelectorAll("#gallery img");
 console.log(gallery);
 modal = document.getElementById("modal");
@@ -15,10 +15,37 @@ for(image of gallery){
     })
 }
 
+/* FERMER LA MODAL */
 window.onclick = function(event) {
     if (event.target == modal) {
       modalImg.style.transform = "scale(0)";
       modal.style.visibility = "hidden";
     }
   }
-    
+document.getElementById("close-modal").addEventListener("click", function(){
+  modalImg.style.transform = "scale(0)";
+  modal.style.visibility = "hidden";
+})
+
+
+  /* OPEN RESPONSIVE MENU */
+document.getElementById("menu").addEventListener("click", function(){
+  document.querySelector("nav").classList.toggle("responsive");
+})
+/* CLOSE MENU */
+document.getElementById("close").addEventListener("click", function(){
+  document.querySelector("nav").classList.toggle("responsive");
+})
+
+/* GALLERY SHOW MORE */
+document.getElementById("load-more").addEventListener("click", function(){
+  document.getElementById("gallery").classList.toggle("load");
+})
+/* GALLERY SHOW LESS */
+document.getElementById("show-less").addEventListener("click", function(){
+  document.getElementById("gallery").classList.toggle("load");
+  var img_content = document.querySelectorAll("#load-content img");
+  img_content.style.opacity="0";
+  document.getElementById("load-content").style.visibility="hidden";
+  img_content.style.visibility="hidden";
+})
